4a0b96dfa33788bf2c36945ff9b84719083c467d
Tu Chung <tu.chung@iag.com.au>
2023-02-02 11:39:14 +1100

    Course 3

---
+++ COURSERA/Python Automating Tasks - Course 3.py
@@ -0,0 +1,41 @@
+
+
+####################
+# Modules
+#
+#####################
+# On Windows
+# Install the following packages whilst OFF VPN:
+#
+# python -m pip install pandas
+#
+# python -m pip install pantab
+#
+# python -m pip install tableauhyperapi
+#
+# python -m pip install tableauserverclient
+#
+#
+# List ALL installed packages:
+#
+# python -m pip list
+#
+#########################
+# How to get Python, PIP working on VPN
+##########################
+# https://confluence.iag.com.au/display/CYS/Netskope+Proxy+Application+configuration+guides+for+developers
+# https://confluence.iag.com.au/display/CYS/Python    -> using Python and netskope
+#
+# As of 2/2/23 , the below commands worked beautifully whilst on vpn
+# python -m pip install --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host files.pythonhosted.org  pandas
+# python -m pip list                  
+##########################
+
+
+# Interpreted Languages include:
+# Python, Ruby, Javascript, Bash, Powershell
+# Interpreted languages trade off slightly slower runtime performance—removing the need for compiling—by using programs called interpreters to execute the code.
+
+# You can run a python script via
+# $ python script.py   OR
+# ./script.py     if the script contains the python SHEBANG #!/usr/bin/env python     
