ea4119ea6fe6066c4079b9f8115c8669990bc80c
Tu Chung <tu.chung@iag.com.au>
2023-02-15 20:09:40 +1100

    reg ex

--- COURSERA/Course2_Regular_Expressions.py
+++ COURSERA/Course2_Regular_Expressions.py
@@ -127,3 +127,37 @@ print(check_web_address("My_Favorite-Blog.US")) # True
 
 
 
+print("Practice Question 2")
+# The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, 
+# followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. 
+# Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
+
+import re
+def check_time(text):
+  pattern = ___
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
+
+
+"""
+print("Practice Question 3")
+import re
+def contains_acronym(text):
+  pattern = ___ 
+  result = re.search(pattern, text)
+  return result != None
+
+print(contains_acronym("Instant messaging (IM) is a set of communication technologies used for text-based communication")) # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication")) # True
+print(contains_acronym("Please do NOT enter without permission!")) # False
+print(contains_acronym("PostScript is a fourth-generation programming language (4GL)")) # True
+print(contains_acronym("Have fun using a self-contained underwater breathing apparatus (Scuba)!")) # True
+
+
+"""
+
